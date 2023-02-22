a4e1ef143441c3d8df331d168ac54d82a4cfc6da
enes3774 <77508537+enes3774@users.noreply.github.com>
2023-02-22 22:25:50 +0300

    Update augmentations.py

--- data_utils/augmentations.py
+++ data_utils/augmentations.py
@@ -1,5 +1,7 @@
 import torchvision as tv
 import open_clip
+from torchvision.transforms import transforms
+from RandAugment import RandAugment
 def _convert_to_rgb(image):
     return image.convert('RGB')
 def get_train_aug(config):
@@ -14,6 +16,16 @@ def get_train_aug(config):
             tv.transforms.ToTensor(),
             tv.transforms.Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(0.26862954, 0.26130258, 0.27577711))
         ])
+    if config.dataset.augmentations == 'augmix':
+        train_augs = tv.transforms.Compose([
+            tv.transforms.RandAugment(2,14),
+            tv.transforms.Resize(size=256,interpolation=tv.transforms.InterpolationMode.BICUBIC, max_size=None, antialias=None),
+            tv.transforms.CenterCrop((224,224)),
+            _convert_to_rgb,
+            tv.transforms.ToTensor(),
+            tv.transforms.Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(0.26862954, 0.26130258, 0.27577711))
+        ])
+        #train_augs.transforms.insert(1, RandAugment(15, 0.5))
     else:
         raise Exception("Unknonw type of augs: {}".format(
             config.dataset.augmentations
@@ -24,7 +36,7 @@ def get_train_aug(config):
 def get_val_aug(config):
     if config.dataset.augmentations_valid == 'default':
         
-        _, _, val_augs = open_clip.create_model_and_transforms('ViT-L-14', pretrained='laion400m_e31')
+        _, _, val_augs = open_clip.create_model_and_transforms(config.model_name, pretrained=config.model_dataset_name)
     else:
         raise Exception("Unknonw type of augs: {}".format(
             config.dataset.augmentations
